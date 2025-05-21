9c2b4de329973908d8ae380410e3ad98803db943
TutaJapos <115644850+TutaJapos@users.noreply.github.com>
2025-05-22 04:43:32 +0700

    Update MainActivity.kt

--- app/src/main/java/ac/id/umn/raqqatamarasanggaiswahyudi/modul1_2/MainActivity.kt
+++ app/src/main/java/ac/id/umn/raqqatamarasanggaiswahyudi/modul1_2/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ac.id.umn.sifanurfaizah.modul1_2.ui.theme.Modul1_2Theme
+import ac.id.umn.raqqatamarasanggaiswahyudi.modul1_2.ui.theme.Modul1_2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
